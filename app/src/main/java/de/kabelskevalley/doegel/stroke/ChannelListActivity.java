6f87f25e1f82f7ef9060306ffee502d20a03cd0c
Felix Rudat <frudat@doegel.de>
2016-05-24 18:23:27 +0200

    implemented pull to refresh list

--- app/src/main/java/de/kabelskevalley/doegel/stroke/ChannelListActivity.java
+++ app/src/main/java/de/kabelskevalley/doegel/stroke/ChannelListActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -50,11 +51,21 @@ public class ChannelListActivity extends AppCompatActivity{
         @Override
         public void onResult(List<Channel> channels) {
             mRecyclerView.setAdapter(new ChannelsRecyclerViewAdapter(channels));
+
+            SwipeRefreshLayout swr = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
+            if (swr != null) {
+                swr.setRefreshing(false);
+            }
         }
 
         @Override
         public void onError(Exception e) {
             Log.e("MainActivity", e.getMessage(), e);
+
+            SwipeRefreshLayout swr = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
+            if (swr != null) {
+                swr.setRefreshing(false);
+            }
         }
     };
 
@@ -75,6 +86,14 @@ public class ChannelListActivity extends AppCompatActivity{
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        SwipeRefreshLayout swr = (SwipeRefreshLayout) findViewById(R.id.refresh_layout);
+        swr.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new HttpChannelTask(mChannelListener).execute();
             }
         });
 
